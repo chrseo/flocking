81f61205906e272e5e13131bf3d6929eec2c22b5
Christopher Seo <christopherseo@berkeley.edu>
2020-05-01 14:47:36 -0700

    window values

--- sketch.js
+++ sketch.js
@@ -1,39 +1,32 @@
-// Flocking Boids
-// Daniel Shiffman
-// https://www.youtube.com/watch?v=mhjuuHl6qHM
-
 const flock = [];
-
-let alignSlider, cohesionSlider, separationSlider, numberSlider;
-
-let quadTree;
+var quadTree;
+var system;
+var alignSlider, cohesionSlider, separationSlider, numberSlider, 
+    perceptionDistSlider, perceptionAngleSlider;
 
 function setup() {
-    createCanvas(640, 360);
-    alignSlider = createSlider(0, 5, 1, 0.1);
+    createCanvas(800, 500);
     cohesionSlider = createSlider(0, 5, 1, 0.1);
-    separationSlider = createSlider(0, 5, 1, 0.1);
-
-    quadTree = new QuadTree(Infinity, 30, new Rect(0, 0, width, height));
-
-    let numBoids = 100;
-    for (let i = 0; i < numBoids; i++) {
-        flock.push(new Boid()); 
-    }
+    separationSlider = createSlider(0, 5, 1.1, 0.1);
+    alignSlider = createSlider(0, 5, 1, 0.1);
+    perceptionDistSlider = createSlider(0, 200, 65, 1);
+    perceptionAngleSlider = createSlider(0, Math.PI, Math.PI / 3, Math.PI / 6);
+    system = new Flocks();
 }
 
 function draw() {
-    quadTree.clear()
+    quadTree.clear();
     for (const boid of flock) {
         quadTree.addItem(boid.position.x, boid.position.y, boid);
     }
-
     background(255);
-
-    for (const boid of flock) {
-        boid.edges();
-        boid.flock(flock);
-        boid.update();
-        boid.show(flock);
-    }
+    fill(6, 43, 75);
+    strokeWeight(0);
+    text("cohesion: " + cohesionSlider.value(), 10, 10);
+    text("separation: " + separationSlider.value(), 10, 25);
+    text("alignment: " + alignSlider.value(), 10, 40);
+    text("perception distance: " + perceptionDistSlider.value(), 10, 55);
+    text("perception angle: " + 
+        Math.round(perceptionAngleSlider.value() * (180/Math.PI)) + "°", 10, 70);
+    system.simulate();
 }
