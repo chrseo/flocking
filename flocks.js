cb72eb2b7300d80748013da1d1fe8c979a548343
Christopher Seo <christopherseo@berkeley.edu>
2020-05-01 14:46:25 -0700

    class to clean up main

---
+++ flocks.js
@@ -0,0 +1,18 @@
+class Flocks {
+    constructor() {
+        quadTree = new QuadTree(Infinity, 30, new Rect(0, 0, width, height));
+        let numBoids = 100;
+        for (let i = 0; i < numBoids; i++) {
+            flock.push(new Boid()); 
+        }
+    }
+
+    simulate() {
+        for (const boid of flock) {
+            boid.edges();
+            boid.flock(flock);
+            boid.update();
+            boid.display(flock);
+        }
+    }
+}
