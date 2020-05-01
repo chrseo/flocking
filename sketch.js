1e72ee2d51bec71027451aaf33d1efe90d007140
Christopher Seo <christophseo@gmail.com>
2020-05-01 15:03:17 -0700

    slider styling

--- sketch.js
+++ sketch.js
@@ -7,10 +7,25 @@ var alignSlider, cohesionSlider, separationSlider, numberSlider,
 function setup() {
     createCanvas(800, 500);
     cohesionSlider = createSlider(0, 5, 1, 0.1);
+        cohesionLabel = createDiv('Cohesion ');
+        cohesionLabel.position(10, 510);  
+        cohesionSlider.parent(cohesionLabel);
     separationSlider = createSlider(0, 5, 1.1, 0.1);
+        separationLabel = createDiv('Separation ');
+        separationLabel.position(10, 530);  
+        separationSlider.parent(separationLabel);
     alignSlider = createSlider(0, 5, 1, 0.1);
+        alignLabel = createDiv('Alignment ');
+        alignLabel.position(10, 550);  
+        alignSlider.parent(alignLabel);
     perceptionDistSlider = createSlider(0, 200, 65, 1);
+        perceptionDistLabel = createDiv('Perception distance ');
+        perceptionDistLabel.position(10, 570);  
+        perceptionDistSlider.parent(perceptionDistLabel);
     perceptionAngleSlider = createSlider(0, Math.PI, Math.PI / 3, Math.PI / 6);
+        perceptionAngleLabel = createDiv('Perception angle ');
+        perceptionAngleLabel.position(10, 590);  
+        perceptionAngleSlider.parent(perceptionAngleLabel);
     system = new Flocks();
 }
 
@@ -19,14 +34,19 @@ function draw() {
     for (const boid of flock) {
         quadTree.addItem(boid.position.x, boid.position.y, boid);
     }
+    cohesionSlider.html();
+    separationSlider.html();
+    alignSlider.html();
+    perceptionDistSlider.html();
+    perceptionAngleSlider.html();
     background(255);
     fill(6, 43, 75);
     strokeWeight(0);
-    text("cohesion: " + cohesionSlider.value(), 10, 10);
-    text("separation: " + separationSlider.value(), 10, 25);
-    text("alignment: " + alignSlider.value(), 10, 40);
-    text("perception distance: " + perceptionDistSlider.value(), 10, 55);
-    text("perception angle: " + 
+    text("Cohesion: " + cohesionSlider.value(), 10, 10);
+    text("Separation: " + separationSlider.value(), 10, 25);
+    text("Alignment: " + alignSlider.value(), 10, 40);
+    text("Perception distance: " + perceptionDistSlider.value(), 10, 55);
+    text("Perception angle: " + 
         Math.round(perceptionAngleSlider.value() * (180/Math.PI)) + "°", 10, 70);
     system.simulate();
 }
